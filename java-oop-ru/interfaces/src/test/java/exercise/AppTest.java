28c43c3c35f86cf1ef5fe9d0681a1a7ef0566ce0
@hexlet/cli <support@hexlet.io>
2024-08-26 19:07:32 +0300

    submit java-oop-ru/interfaces

--- java-oop-ru/interfaces/src/test/java/exercise/AppTest.java
+++ java-oop-ru/interfaces/src/test/java/exercise/AppTest.java
@@ -57,6 +57,36 @@ class AppTest {
     }
 
     // BEGIN
-    
+    @Test
+    void testReversedSequence1() {
+        CharSequence text = new ReversedSequence("abcdef");
+        String expected = "fedcba";
+        String result = text.toString();
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void testReversedSequence2() {
+        CharSequence text = new ReversedSequence("abcdef");
+        char expected = 'e';
+        char result = text.charAt(1);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void testReversedSequence3() {
+        CharSequence text = new ReversedSequence("abcdef");
+        int expected = 6;
+        int result = text.length();
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void testReversedSequence4() {
+        CharSequence text = new ReversedSequence("abcdef");
+        String expected = "edc";
+        String result = text.subSequence(1, 4).toString();
+        assertThat(result).isEqualTo(expected);
+    }
     // END
 }
