b447316c587f7bac34e5842e23b29c742b757359
@hexlet/cli <support@hexlet.io>
2024-09-25 23:33:19 +0500

    submit java-oop-ru/subtyping

--- java-oop-ru/subtyping/src/test/java/exercise/FileKVTest.java
+++ java-oop-ru/subtyping/src/test/java/exercise/FileKVTest.java
@@ -25,7 +25,7 @@ class FileKVTest {
 
     // BEGIN
     @Test
-    void FileKVTest1() {
+    void aFileKVTest() {
         KeyValueStorage storage = new FileKV("src/test/resources/file", Map.of("key", "value"));
         assertThat(storage.get("key2", "default")).isEqualTo("default");
         assertThat(storage.get("value", "default")).isEqualTo("key");
