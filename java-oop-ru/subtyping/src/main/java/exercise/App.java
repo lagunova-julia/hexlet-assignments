c4719890f68e97a02399e320ecee008bc98777fb
@hexlet/cli <support@hexlet.io>
2024-10-10 22:08:46 +0500

    submit java-oop-ru/subtyping

--- java-oop-ru/subtyping/src/main/java/exercise/App.java
+++ java-oop-ru/subtyping/src/main/java/exercise/App.java
@@ -1,25 +1,15 @@
 package exercise;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 // BEGIN
 public class App {
     public static void swapKeyValue(KeyValueStorage storage) {
-        Map<String, String> originalMap = storage.toMap();
-        Map<String, String> swappedStorage = new HashMap<>();
-
-        for (Map.Entry<String, String> entry : originalMap.entrySet()) {
-            String key = entry.getKey();
-            String value = entry.getValue();
-            swappedStorage.put(value, key);
-        }
-
-        if (storage instanceof InMemoryKV) {
-            storage = new InMemoryKV(swappedStorage);
-        } else {
-            storage = new FileKV("src/test/resources/file", swappedStorage);
-        }
+        Map<String, String> data = storage.toMap();
+        Set<Map.Entry<String, String>> entries = data.entrySet();
+        entries.forEach(entry -> storage.unset(entry.getKey()));
+        entries.forEach(entry -> storage.set(entry.getValue(), entry.getKey()));
     }
 }
 // END
